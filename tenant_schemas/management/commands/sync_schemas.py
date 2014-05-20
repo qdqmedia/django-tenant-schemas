e2eb47181f65e54d065ce0f9ae7056354bf7c6b8
Christian Felipe <cfelipe@qdqmedia.com>
2014-05-20 18:03:21 +0200

    implement shared models by creating views for public
    A new settings SHARED_MODELS is set to stablish if a model should be
    shared. If any, it is not 'django managed' when syncing databases and
    instead of creating the table into the schema a view to the table on
    public schema is created.

--- tenant_schemas/management/commands/sync_schemas.py
+++ tenant_schemas/management/commands/sync_schemas.py
@@ -20,9 +20,13 @@ class Command(SyncCommon):
         if "south" in settings.INSTALLED_APPS:
             self.options["migrate"] = False
 
-        # save original settings
-        for model in get_models(include_auto_created=True):
+        for app, model in self._iter_model_apps():
+            # save original settings
             setattr(model._meta, 'was_managed', model._meta.managed)
+            # Set model info to know if the models should be individually shared
+            setattr(model._meta, 'shared_model',
+                    ('.'.join((app.__name__, model.__name__)) in
+                     getattr(settings, 'SHARED_MODELS', ())))
 
         ContentType.objects.clear_cache()
 
@@ -35,24 +39,36 @@ class Command(SyncCommon):
         for model in get_models(include_auto_created=True):
             model._meta.managed = model._meta.was_managed
 
-    def _set_managed_apps(self, included_apps):
+    def _iter_model_apps(self, include_auto_created=True):
+        """Iters for each (app, model)"""
+        for app_model in get_apps():
+            for model in get_models(app_model, include_auto_created=True):
+                yield (app_model, model)
+
+    def _set_managed_apps(self, included_apps, tenant=True):
         """ sets which apps are managed by syncdb """
         for model in get_models(include_auto_created=True):
             model._meta.managed = False
 
         verbosity = int(self.options.get('verbosity'))
-        for app_model in get_apps():
-            app_name = app_model.__name__.replace('.models', '')
-            if app_name in included_apps:
-                for model in get_models(app_model, include_auto_created=True):
-                    model._meta.managed = model._meta.was_managed
-                    if model._meta.managed and verbosity >= 3:
-                        self._notice("=== Include Model: %s: %s" % (app_name, model.__name__))
+        for app, model in self._iter_model_apps():
+            app_name = app.__name__.replace('.models', '')
+            if (app_name in included_apps and
+                    not (tenant and model._meta.shared_model)):
+                model._meta.managed = model._meta.was_managed
+                if model._meta.managed and verbosity >= 3:
+                    self._notice("=== Include Model: %s: %s" % (app_name, model.__name__))
 
     def _sync_tenant(self, tenant):
         self._notice("=== Running syncdb for schema: %s" % tenant.schema_name)
         connection.set_tenant(tenant, include_public=False)
         SyncdbCommand().execute(**self.options)
+        for model in get_models():
+            if model._meta.shared_model:
+                cursor = connection.cursor()
+                cursor.execute(
+                    'CREATE VIEW {schema}.{table} AS SELECT * FROM public.{table}'.format(
+                        schema=tenant.schema_name, table=model._meta.db_table))
 
     def sync_tenant_apps(self, schema_name=None):
         apps = self.tenant_apps or self.installed_apps
@@ -70,6 +86,6 @@ class Command(SyncCommon):
 
     def sync_public_apps(self):
         apps = self.shared_apps or self.installed_apps
-        self._set_managed_apps(apps)
+        self._set_managed_apps(apps, tenant=False)
         SyncdbCommand().execute(**self.options)
         self._notice("=== Running syncdb for schema public")
